728a0796cd133594b24caadf1b84b8e32bb6e908
JeanBilheux <bilheuxjm@ornl.gov>
2017-09-26 12:46:41 -0400

    Trying to add notebook to documentation

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -31,7 +31,14 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+#extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc',
+              'nbsphinx',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.githubpages',
+              'IPython.sphinxext.ipython_console_highlighting']
+
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,7 +47,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.ipynb']
 
 # The master toctree document.
 master_doc = 'index'
@@ -69,7 +76,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = []
+exclude_patterns = ['_build','**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
