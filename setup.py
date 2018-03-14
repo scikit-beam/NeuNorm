0cb2213fb1e9c1c06a20892c3c700be006b428b2
JeanBilheux <bilheuxjm@ornl.gov>
2018-03-13 21:45:59 -0400

    Improved setup.py file

--- setup.py
+++ setup.py
@@ -1,35 +1,90 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+import os
+import sys
+from shutil import rmtree
+
 import NeuNorm
 
+NAME = 'NeuNorm'
+DESCRIPTION = "neutron normalization data"
+LONGDESCRIPTION = "See the README.md file on GitHub for more information"
+URL = "https://github.com/ornlneutronimaging/NeuNorm"
+EMAIL = "bilheuxjm@ornl.gov"
+AUTHOR = "Jean Bilheux"
+VERSION = NeuNorm.__version__
+KEYWORDS = "neutron normalization imaging"
+
+# what packages are required for this module to be executed
+REQUIRED = ['numpy',
+            'pillow',
+            'pathlib',
+            'astropy',
+            'scipy',
+            ]
+
+here = os.path.abspath('./')
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        """Initialization options."""
+        pass
+
+    def finalize_options(self):
+        """Finalize options."""
+        pass
+
+    def run(self):
+        """Remove previous builds."""
+        try:
+            self.status('Removing previous builds...')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel distribution...')
+        os.system('{0} setup.py sdist bdist_wheel'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine...')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
 setup(
-    name = "NeuNorm",
-    version = NeuNorm.__version__,
-    author = "Jean Bilheux",
-    author_email = "bilheuxjm@ornl.gov",
+    name = NAME,
+    description = DESCRIPTION ,
+    long_description = LONGDESCRIPTION,
+    url = URL,
+    version = VERSION,
+    author = AUTHOR,
+    author_email = EMAIL,
     packages = find_packages(exclude=['tests', 'notebooks']),
     include_package_data = True,
     test_suite = 'tests',
-    install_requires = [
-        'numpy',
-        'pillow',
-        'pathlib',
-        'astropy',
-        'scipy',
-    ],
+    install_requires = REQUIRED,
     dependency_links = [
     ],
-    description = "neutron normalization data",
     license = 'BSD',
-    keywords = "neutron normalization imaging",
-    url = "https://github.com/ornlneutronimaging/NeuNorm",
+    keywords = KEYWORDS,
     classifiers = ['Development Status :: 3 - Alpha',
                    'Topic :: Scientific/Engineering :: Physics',
                    'Intended Audience :: Developers',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6'],
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
-
 
 # End of file
