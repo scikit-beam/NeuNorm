0b6509e353921279ab3e614643ccca6cfec1a954
JeanBilheux <bilheuxjm@ornl.gov>
2024-01-03 09:51:46 -0500

    Added language and changed ** to *. this refs #14

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -60,7 +60,7 @@ author = 'Jean Bilheux'
 # built documents.
 #
 # The short X.Y version.
-version = '1'
+version = '1.6.3'
 # The full version, including alpha/beta/rc tags.
 release = '0'
 
@@ -69,12 +69,12 @@ release = '0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build','**.ipynb_checkpoints', '**.ipynb']
+exclude_patterns = ['_build', '*.ipynb_checkpoints', '*.ipynb']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
